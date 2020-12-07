c8ccf331ca9ab2d93b7c71e1fcd30dc0177c301b
TamannaSarker <67562599+TamannaSarker@users.noreply.github.com>
2020-12-07 12:14:05 +0100

    just

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 
       document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
 
-      var valueCount
+      var valueCount=0
       //plus
-      document.querySelector("#plus-btn").addEventListener("click", function () {
-         valueCount = document.getElementById("quantity").value;
+     const btnList =  document.querySelectorAll(".plus-btn")
+      console.log("button" , btnList)
 
-         valueCount++;
+      for( var i = 0;i<btnList.length; i++ ){
 
+         
+            btnList[i].addEventListener("click", function (e) {
+               e.preventDefault()
+              var  val =document.querySelector(".input").value 
+             
+              
+               var value =  Number(val)
+               value += 1
+               // val = val+1
+               // valueCount +
+               document.querySelector(".input").value = value
+      
+           })
+           console.log("hello" , valueCount)
+
+         }
+     
 
          document.getElementById("quantity").value = valueCount
          if (valueCount > 1) {
@@ -15,12 +32,20 @@
             document.querySelector("#minus-btn").classList.remove("disabled")
          }
 
-      })
+   
       //minus
-      document.querySelector("#minus-btn").addEventListener("click", function () {
-         valueCount = document.getElementById("quantity").value;
 
-         valueCount--;
+
+     const btnListMinus= document.querySelectorAll(".minus-btn")
+      
+      btnListMinus.map(  btn =>{
+         btn.addEventListener("click", function () {
+            valueCount = document.getElementById("quantity").value;
+   
+            valueCount--;
+      })
+      
+     
 
 
          document.getElementById("quantity").value = valueCount
