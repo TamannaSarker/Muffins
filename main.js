d24c7e507ea7a4a7b101bdea38c924e97ba2f366
Jyoti <jyoti.nambiar@medieinstitutet.se>
2020-12-09 12:23:03 +0100

    Added the javascript for cart

--- main.js
+++ main.js
@@ -1,82 +1,149 @@
-/*Cart increment function*/
-let buyBtn = document.querySelectorAll(".link-button");
-console.log(buyBtn);
 
-for (var i = 0; i < buyBtn.length; i++) {
-   buyBtn[i].addEventListener("click", addCartCounter)
-}
-
-function updateCartOnReload() {
-   let productCount = localStorage.getItem("cartNumber");
-   document.querySelector(".cart span").textContent = productCount;
-
-}
+//------------ LOGIN SECTION -------------------------
 
 
-function addCartCounter() {
-   let productCount = localStorage.getItem("cartNumber");
-   productCount = parseInt(productCount);
-   if (productCount) {
-      localStorage.setItem('cartNumber', productCount + 1);
-      document.querySelector(".cart span").textContent = productCount + 1;
-   } else {
-      localStorage.setItem('cartNumber', 1);
-      document.querySelector(".cart span").textContent = 1;
+function validate() {
+   var username = document.getElementById("username").value;
+   var password = document.getElementById("password").value;
+
+   if (username == "admin" && password == "123") {
+      alert("Login succesful");
+      return false;
+
    }
-
-}
-
-updateCartOnReload();
-
-
-      //------------ LOGIN SECTION -------------------------
-
-
-      function validate(){
-         var username = document.getElementById("username").value;
-         var password = document.getElementById("password").value;
-
-         if(username =="admin" && password == "123"){
-            alert("Login succesful");
-            return false;
-
-         }
-         else{
-            alert("Login failed");
-         }
-document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
-
-var valueCount
-//plus
-document.querySelector("#plus-btn").addEventListener("click", function () {
-   valueCount = document.getElementById("quantity").value;
-
-   valueCount++;
-
-
-   document.getElementById("quantity").value = valueCount
-   if (valueCount > 1) {
-      document.querySelector("#minus-btn").removeAttribute("disabled")
-      document.querySelector("#minus-btn").classList.remove("disabled")
+   else {
+      alert("Login failed");
    }
+   document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
 
-})
-//minus
-document.querySelector("#minus-btn").addEventListener("click", function () {
-   valueCount = document.getElementById("quantity").value;
+   var valueCount
+   //plus
+   document.querySelector("#plus-btn").addEventListener("click", function () {
+      valueCount = document.getElementById("quantity").value;
 
-   valueCount--;
-
-
-   document.getElementById("quantity").value = valueCount
-   if (valueCount == 1) {
-      document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
-   }
+      valueCount++;
 
 
-
-});
-
+      document.getElementById("quantity").value = valueCount
+      if (valueCount > 1) {
+         document.querySelector("#minus-btn").removeAttribute("disabled")
+         document.querySelector("#minus-btn").classList.remove("disabled")
       }
 
-      //------------ LOGIN SECTION END -------------------------
+   })
+   //minus
+   document.querySelector("#minus-btn").addEventListener("click", function () {
+      valueCount = document.getElementById("quantity").value;
+
+      valueCount--;
+
+
+      document.getElementById("quantity").value = valueCount
+      if (valueCount == 1) {
+         document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
+      }
+
+
+
+   });
+
+}
+
+//------------ LOGIN SECTION END -------------------------
+
+//cart adding function
+/*cart display function*/
+(function () {
+   /*select the cart link and the cart div and add a click event listner to show the cart by adding the css class using classList function, toggle helps in removing the css class when we again click on the cart link */
+   const cartInfo = document.querySelector("#cart-info");
+   const cart = document.querySelector("#cart");
+
+   cartInfo.addEventListener("click", function () {
+      cart.classList.toggle("show-cart");
+
+
+   });
+
+})();
+
+//add items to the cart
+
+(function () {
+   const cartBtn = document.querySelectorAll(".link-button");
+   cartBtn.forEach(function (btn) {
+      btn.addEventListener("click", function (event) {
+         let fullPath = event.target.parentElement.parentElement.previousElementSibling.src;
+         let pos = fullPath.indexOf("images") + 6;
+         let partPath = fullPath.slice(pos);
+         const item = {};
+         item.img = `img-cart${partPath}`;
+         let name = event.target.parentElement.previousElementSibling.children[0].textContent;
+         item.name = name;
+         let price = event.target.parentElement.children[0].textContent;
+         let finalPrice = price.slice("1");
+         item.price = finalPrice;
+         console.log(item);
+         //local storage function start
+
+         localStorage.setItem("cartItem", JSON.stringify({ item }));
+
+         localStorage.getItem("cartItem");
+
+         //local storage function end
+
+
+         const cartItem = document.createElement("div");
+         cartItem.classList.add("cart-item");
+         cartItem.innerHTML = `
+         <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+             <div class="item-text">
+                 <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                 <span>$</span>
+                 <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                 
+             </div>
+             <a href="#" id='cart-item-remove' class="cart-item-remove">
+                 <i class="fas fa-trash"></i>
+             </a>
+
+             </div>
+             
+             `;
+         //select cart div
+         const cart = document.querySelector(".cart");
+         const total = document.querySelector(".cart-total-container");
+
+         cart.insertBefore(cartItem, total);
+         alert("item added to cart");
+         showTotal();
+
+      });
+
+   });
+
+   function showTotal() {
+      const items = document.querySelectorAll(".cart-item-price");
+      const total = [];
+      items.forEach(function (item) {
+         total.push(parseFloat(item.textContent));
+
+      });
+      console.log(total);
+      const totalMoney = total.reduce(function (total, item) {
+         total += item;
+         return total;
+
+      }, 0);
+      const FinalTotal = totalMoney.toFixed(2);
+      const cartTotal = document.querySelector("#cart-total");
+      cartTotal.textContent = FinalTotal;
+
+      const navCartTotal = document.querySelector(".cart-total");
+      navCartTotal.textContent = "$" + FinalTotal;
+
+      const carttotalItem = document.querySelector(".item-total");
+      carttotalItem.textContent = total.length;
+   };
+
+})();
+
