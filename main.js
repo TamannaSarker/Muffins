80a3a06f3217bd05c2ab2b7bd97e40fad0535eb4
Lea <lea.thifa@gmail.com>
2020-12-09 11:26:11 +0100

    Added login.html with styling in styles.css and login section in main.js

--- main.js
+++ main.js
@@ -28,6 +28,24 @@
             document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
          }
 
-
-
       })
+
+      //------------ LOGIN SECTION -------------------------
+
+
+      function validate(){
+         var username = document.getElementById("username").value;
+         var password = document.getElementById("password").value;
+
+         if(username =="admin" && password == "123"){
+            alert("Login succesful");
+            return false;
+
+         }
+         else{
+            alert("Login failed");
+         }
+
+      }
+
+      //------------ LOGIN SECTION END -------------------------
