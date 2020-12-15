670cd6d46c52cc50a9f7de20a9827a38f79c7269
Lea <lea.thifa@gmail.com>
2020-12-15 11:12:57 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*Cart increment function*/
+
+/*
 let buyBtn = document.querySelectorAll(".link-button");
 console.log(buyBtn);
 
@@ -28,22 +29,92 @@ function addCartCounter() {
 
 updateCartOnReload();
 
+*/
+//------------ LOGIN SECTION -------------------------
 
-      //------------ LOGIN SECTION -------------------------
+
+function validate() {
+   var username = document.getElementById("username").value;
+   var password = document.getElementById("password").value;
+
+   if (username == "admin" && password == "123") {
+      alert("Login succesful");
+
+      window.location = "file:///Users/lea/Documents/GitHub/Muffins/admin.html";
+      return false;
+   }
+   else {
+      alert("Login failed");
+   }
+
+   (function () {
+      const loginInformation = document.getElementById("loginInfo");
+      const loginNow = document.getElementById("login");
+
+      loginInformation.addEventListener("click", function () {
+         loginNow.classList.toggle("showLogin");
+      })
+
+   })
+}
 
 
-      function validate(){
-         var username = document.getElementById("username").value;
-         var password = document.getElementById("password").value;
+//------------ LOGIN SECTION END -------------------------
 
-         if(username =="admin" && password == "123"){
-            alert("Login succesful");
-            return false;
 
-         }
-         else{
-            alert("Login failed");
-         }
+//------------ ADMIN SECTION-------------------------
+
+let products = [
+   {
+       name: "Mint Cupcake",
+       price: 15,
+   },
+   {
+       name: "Strawberry Cupcake",
+       price: 10,
+   },
+   {
+       name: "Chocolate Cupcake",
+       price: 5,
+   }
+]
+
+function addNewValue(e) {
+
+   e.preventDefault();
+
+   let newName = document.querySelector("#enterProductName").value;
+   let newPrice = document.querySelector("#enterPrice").value;
+
+   let newInput = {
+      name: newName,
+      price: newPrice,
+   }
+
+   let adminContainer = document.querySelector(".adminContainer");
+   if (adminContainer) {
+
+      const divNewName = document.querySelector(".product-name-style");
+      divNewName.innerHTML = newName;
+      
+      const divNewPrice = document.querySelector(".product-price-style");
+      divNewPrice.innerHTML = newPrice;
+
+      products.push(newInput);
+
+      console.log("This are my items: ", products);
+
+   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+   document.querySelector("#submit").addEventListener("click", addNewValue);
+})
+
+//------------ ADMIN SECTION END -------------------------
+
+/*
+
 document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
 
 var valueCount
@@ -73,10 +144,7 @@ document.querySelector("#minus-btn").addEventListener("click", function () {
       document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
    }
 
-
-
 });
+*/
 
-      }
 
-      //------------ LOGIN SECTION END -------------------------
