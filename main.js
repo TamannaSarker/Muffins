e650b9e46d5b194464f6e3769e84e63f00137b88
TamannaSarker <67562599+TamannaSarker@users.noreply.github.com>
2020-12-18 14:43:58 +0100

    added new code

--- main.js
+++ main.js
@@ -14,49 +14,49 @@ function validate() {
    else {
       alert("Login failed");
    }
-   document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
+   //document.querySelector("#minus-btn").setAttribute("disabled", "disabled");
 
-   var valueCount
+   //var valueCount
    //plus
-   document.querySelector("#plus-btn").addEventListener("click", function () {
-      valueCount = document.getElementById("quantity").value;
+   //document.querySelector("#plus-btn").addEventListener("click", function () {
+      //valueCount = document.getElementById("quantity").value;
 
-      valueCount++;
+      //valueCount++;
 
-      var valueCount
+      //var valueCount
       //plus
 
-      document.querySelector("#plus-btn").addEventListener("click", function () {
-         valueCount = document.getElementById("quantity").value;
+      //document.querySelector("#plus-btn").addEventListener("click", function () {
+         //valueCount = document.getElementById("quantity").value;
 
-         document.getElementById("quantity").value = valueCount
-         if (valueCount > 1) {
-            document.querySelector("#minus-btn").removeAttribute("disabled")
-            document.querySelector("#minus-btn").classList.remove("disabled")
-         }
+         //document.getElementById("quantity").value = valueCount
+         //if (valueCount > 1) {
+            //document.querySelector("#minus-btn").removeAttribute("disabled")
+            //document.querySelector("#minus-btn").classList.remove("disabled")
+         //}
 
-      })
+      //})
       //minus
-      document.querySelector("#minus-btn").addEventListener("click", function () {
-         valueCount = document.getElementById("quantity").value;
+      //document.querySelector("#minus-btn").addEventListener("click", function () {
+         //valueCount = document.getElementById("quantity").value;
 
-         valueCount--;
-
-
-         document.getElementById("quantity").value = valueCount
-         if (valueCount == 1) {
-            document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
-         }
+         //valueCount--;
 
 
-
-      });
+         //document.getElementById("quantity").value = valueCount
+         //if (valueCount == 1) {
+           // document.querySelector("#minus-btn").setAttribute("disabled", "disabled")
+         //}
 
 
 
-   });
+      //});
 
-}
+
+
+   //});
+
+//}
 //------------ LOGIN SECTION END -------------------------
 
 
