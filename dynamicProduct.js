f9ee2bd2d1807bd1892a66cebca5660161f50445
Lea <lea.thifa@gmail.com>
2020-12-28 10:24:07 +0100

    Revert "test"
    This reverts commit 117025f66f6b5de6a83a3aa4646b76d6607296d6, reversing
    changes made to cd4332efc09811fe9c963308dba76606d9be7b1c.

--- dynamicProduct.js
+++ dynamicProduct.js
@@ -1,80 +1,7 @@
 window.onload = function () {
 
-    const chooseImgBtn = document.querySelector("#chooseImg");
-    chooseImgBtn.addEventListener("click", apiImageFunc);
-
-
-
-
-    //function to call images from API
-
-    async function apiImageFunc(e) {
-        e.preventDefault();
-
-        let apiImgDiv = document.querySelector(".apiImageDiv");
-        apiImgDiv.classList.add("show-apiImageDiv");
-
-        const closeBtn = document.querySelector(".fa-times-circle");
-        closeBtn.addEventListener("click", function () {
-            apiImgDiv.classList.remove("show-apiImageDiv");
-        });
-
-        const url = "https://api.unsplash.com/search/photos?query=cupcakes&per_page=20&client_id=3JCiE4B5MmNqBKSapU83_51udslQFpkOw_ObgTDTXO8";
-        const imageDiv = document.querySelector('.image');
-        await fetch(url)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-
-                for (let i = 0; i < data.results.length; i++) {
-
-
-                    let imageElement = document.createElement('img');
-                    imageElement.setAttribute("class", "apiImg");
-                    imageElement.src = data.results[i].urls.thumb;
-                    apiImgDiv.append(imageElement);
-
-                }
-            });
-
-        //To select the image chosen and view the source in a div
-        let fullPath = document.getElementsByClassName("apiImg");
-
-        console.log("api images", fullPath);
-
-        let imgClicked;
-
-        console.log("fullpath len", fullPath.length);
-
-        for (let i = 0; i < fullPath.length; i++) {
-
-            fullPath[i].addEventListener("click", () => {
-                imgClicked = fullPath[i].currentSrc;
-
-                console.log("im in loop", imgClicked);
-
-                alert("image chosen");
-                let newdivElement = document.createElement("div");
-                newdivElement.setAttribute("class", "imgSource");
-                newdivElement.textContent = imgClicked;
-                let formelement = document.querySelector("form");
-                formelement.append(newdivElement);
-
-            });
-        }
-
-
-
-    }
-
-
-
-
     const addItembtn = document.querySelector("#addBtn");
-
     addItembtn.addEventListener("click", loadImg);
-
 
 
     //adding new prodcuts to product.html from addProduct.html
@@ -92,33 +19,25 @@ window.onload = function () {
         let dataFromLocalStorage = JSON.parse(localStorage.getItem("ourProducts"));
         (function addNewProductToLocalStorage() {
 
-            console.log("im here");
-
 
             //pushing the new product object to the existing data in local storage
             let item = {
             };
             let itemName = document.querySelector("#itemName").value;
             let price = document.querySelector("#itemPrice").value;
-            let imgSourceElement = document.querySelector(".imgSource").textContent;
-
-
+            let fullPath = document.querySelector("#newImgSrc").value;
+            console.log(fullPath);
+            let position = fullPath.indexOf("fakepath") + 9;
+            let partPath = fullPath.slice(position);
+            let imgSrc = `images/${partPath}`;
             item.id = id;
             item.itemName = itemName;
             item.price = price;
-
-
-
-            item.imgSrc = imgSourceElement;
-
-            console.log("i am the Object", item);
+            item.imgSrc = imgSrc;
             dataFromLocalStorage.push(item);
 
-
         })();
-        id++;
-
-        // updating the id to next increment value
+        id++; // updating the id to next increment value
         //updating the new value to localstorage
         localStorage.setItem("ourProducts", JSON.stringify(dataFromLocalStorage));
         //updating the new increment of id to localstorage
@@ -133,18 +52,18 @@ window.onload = function () {
 if (JSON.parse(localStorage.getItem("ourProducts")) == null) {
     localStorage.setItem("ourProducts", JSON.stringify(products));
     //window.location.reload();
-}
-else {
+ }
+ else {
     //if localstorage has data creating those products dynamically in product.html
     let currentProducts = JSON.parse(localStorage.getItem("ourProducts"));
 
     console.log(currentProducts);
 
     for (let i = 0; i < currentProducts.length; i++) {
-        let cardContainersDiv = document.querySelector(".card-containers");
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("card");
-        newDiv.innerHTML += `
+       let cardContainersDiv = document.querySelector(".card-containers");
+       let newDiv = document.createElement("div");
+       newDiv.classList.add("card");
+       newDiv.innerHTML += `
        <img class="img-style" src="${currentProducts[i].imgSrc}" alt="Julmuffin">
 
        <div class="card-body">
@@ -158,7 +77,7 @@ else {
                <button id= ${currentProducts[i].id} class="remove-item-button" onclick= "deleteItem(this)">Remove</button>
            </div>`;
 
-        cardContainersDiv.append(newDiv);
+       cardContainersDiv.append(newDiv);
     }
  }
 
@@ -173,8 +92,8 @@ else {
             
                 localStorage.setItem("ourProducts", JSON.stringify(item));
         }
-        location.reload();
-
+        location.reload(); 
+        
     }
 }
 
